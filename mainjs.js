d266ea7d085103d174aaea890d4972c10437634e
Stephan Taylor <stephantaylor21@hotmail.com>
2022-06-14 17:52:06 -0600

    played around with javascript

--- mainjs.js
+++ mainjs.js
@@ -1,19 +1,28 @@
-const mainArticle = document.querySelector("[main-article]");
+const mainArticle = document.querySelector(".main-article");
 const cardSteps = [...mainArticle.querySelectorAll("[card-step]")];
 
-let currentStep = cardSteps.findIndex((step) => {
+let currentStep = cardSteps.findIndex(step => {
   return step.classList.contains("active");
 });
 
 if (currentStep < 0) {
   currentStep = 0;
-  cardSteps[currentStep].classList.add("active");
+  showCurrentStep()
 }
 
-mainArticle.addEventListener("click", (e) => {
+mainArticle.addEventListener("click", e => {
   if (e.target.matches("[button-next]")) {
-    currentStep += 1;
+    currentStep += 1
   } else if (e.target.matches("[button-previous]")) {
-    currentStep -= 1;
-  }
-});
+    currentStep -= 1
+  } 
+  showCurrentStep()
+})
+console.log(cardSteps)
+
+function showCurrentStep() {
+  cardSteps.forEach((step,index) => {
+    step.classList.toggle("active", index === currentStep)
+
+  })
+}
